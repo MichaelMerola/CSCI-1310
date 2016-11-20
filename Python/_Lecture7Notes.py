9a81e16099e76ed36c6a5a7009f6aac5ac4d0fa3
Michael Merola <michaelr.merola@gmail.com>
2016-11-19 19:17:51 -0700

    Quick update

--- Python/_Lecture7Notes.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-imgDolphin = mpimg.imread('dolphin_swimming.jpg')
-print imgDolphin
-imgPLdolphin = plt.imshow(imgDolphin)
-plt.show()
-
-print imgDolphin.shape
