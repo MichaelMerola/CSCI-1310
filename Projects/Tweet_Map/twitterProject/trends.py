0b9711d4ca79bbb7c0e2a4bd1c679e5c8631ae94
Michael Merola <michaelr.merola@gmail.com>
2016-12-02 15:18:24 -0700

    Updated Projects Folder

---
+++ Projects/Tweet_Map/twitterProject/trends.py
@@ -0,0 +1,104 @@
+import sys
+import json
+from geo import GeoPosition
+from tweet import Tweet
+from state import load_states
+from country import Country
+from parse import load_sentiments
+from colors import get_sentiment_color
+
+class SentimentAnalysis:
+	def __init__(self):
+		self.sentiments = load_sentiments()
+		self.states = load_states()
+	def showCountry(self):
+		self.usa = Country(self.states, 1200)
+	def setSentiment(self, code, score):
+		for state in self.states:
+			if state.abbrev() == code:
+				state.addSentiments(score)
+
+	def analyzeData(self, fileName, query):
+		
+		rawJSONTweetData = open(fileName, "r")
+		tweetLibrary = []
+
+		#import data into classes
+		for line in rawJSONTweetData:
+			rawTweetData = json.loads(line)
+			tweetMessage = rawTweetData["text"]
+			tweetTime = rawTweetData["created_at"]
+			tweetLocation = GeoPosition(rawTweetData['coordinates'][1], rawTweetData['coordinates'][0])
+			if (query in tweetMessage):
+				tweetLibrary.append(Tweet(tweetMessage, tweetTime, tweetLocation))
+		'''end import for'''
+
+		#parse through every tweet to get a sentiment score
+		for twt in tweetLibrary:
+			tweetTxt = twt.message().split(" ")
+			tweetGeo = twt.position()
+			shortDist = -1
+			closestState = " "
+			sentScore = 0
+			count = 1
+
+			for state in self.states:
+				dist = tweetGeo.distance(state.centroid())
+				if shortDist == -1:
+					shortDist = dist
+					closestState = state.abbrev()
+
+				elif dist < shortDist:
+					shortDist = dist
+					closestState = state.abbrev()
+
+
+			for word in tweetTxt:
+				if word in self.sentiments:
+					sentScore += self.sentiments[word]
+					count += 1
+
+			#print closestState
+			avgScore = sentScore/count
+			#print avgScore
+
+			self.setSentiment(closestState, avgScore)
+		'''end for'''
+		
+	'''end analyzeData'''           
+    
+#finish
+
+
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		query = ' '.join(sys.argv[1:])
+		print "Search Query: " + query + "\n"
+	else:
+		print "error: no query"
+		
+	sa = SentimentAnalysis()
+
+	fileName = "tweets_with_time.json"
+	sa.analyzeData(fileName, query)
+	print "Loading DataSet1..."
+	
+	fileName = "tweets_with_time_2.json"
+	sa.analyzeData(fileName, query)
+	print "Loading DataSet2..."
+	
+	fileName = "tweets_with_time_3.json"
+	sa.analyzeData(fileName, query)
+	print "Loading DataSet3..."
+	
+	fileName = "tweets_with_time_4.json"
+	sa.analyzeData(fileName, query)
+	print "Loading DataSet4...\n"
+
+	sa.showCountry()
+
+	for state in sa.states:
+		sa.usa.setFillColor(state.abbrev(), get_sentiment_color(state.getAverageSent()))
+
+#finish
